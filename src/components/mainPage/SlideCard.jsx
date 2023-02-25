e8b5e5345613531f521b3a6cb68474f905ef6f52
abhishek <purohitabhishek308@gmail.com>
2023-02-26 01:44:48 +0530

    day3

--- src/components/mainPage/SlideCard.jsx
+++ src/components/mainPage/SlideCard.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import Sdata from './Sdata'
 
 const SlideCard = () => {
   return (
-    <div>
-      <h1>
-          SlideCard
-      </h1>
-    </div>
+    <>
+     {Sdata.map ((value, index) => {
+          return (
+               <div className="box d_flex top" key = {index}>
+                    <div className="left">
+                         <h1>{value.title}</h1>    
+                         <p>{value.desc}</p>
+                         <button className='btn-primary'>Visit collections</button>
+                    </div>
+                    <div className="right">
+                         <img src={value.cover} alt=''/>
+                    </div>
+               </div>
+               )
+     })}
+          
+    </>
   )
 }
 
